032b560411aa8778a522b8c9966984189b9129ed
wawrzaszek <szymonmosor@outlook.com>
2026-05-11 10:26:15 +0200

    data js

--- skrypt.js
+++ skrypt.js
@@ -7,3 +7,14 @@ setInterval(() => {
     const sekunda = String(data.getSeconds()).padStart(2, '0');
     zegar.textContent = `${godzina}:${minuta}:${sekunda}`;
 }, 500);
+
+
+const dataElement = document.querySelector('data');
+
+setInterval(() => {
+    const data = new Date();
+    const rok = String(data.getFullYear()).padStart(4, '0');
+    const miesiac = String(data.getMonth() + 1).padStart(2, '0');
+    const dzien = String(data.getDate()).padStart(2, '0');
+    dataElement.textContent = `${rok}-${miesiac}-${dzien}`;
+}, 1000);
